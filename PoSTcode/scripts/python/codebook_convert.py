aa3d96062bd39a763d413983d422ae247b8f4a45
Tong LI <tl10@sanger.ac.uk>
2024-08-23 14:55:57 +0000

    add postcode scripts

---
+++ PoSTcode/scripts/python/codebook_convert.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+import fire
+import pandas as pd
+import re
+import numpy as np
+from collections import OrderedDict
+
+
+VERSION = "0.0.1"
+def main(csv_file,
+         channel_map:dict={"Cy5": "A", "AF488": "G", "Cy3": "C", "Atto425": "T", "AF750":"T"}, # This is orderd! be cautious !!!
+         sep=","):
+    channel_map = OrderedDict(channel_map)
+    print(channel_map)
+    if csv_file.endswith(".xlsx"):
+        d = pd.read_excel(csv_file)
+    else:
+        d = pd.read_csv(csv_file, sep=sep)
+    code_sizes = [len(str(c)) for c in d.code]
+    n_cycle_list = np.unique(code_sizes)
+    assert len(n_cycle_list) == 1
+    channel_info = {}
+    channel_info["nCycles"] = n_cycle_list[0]
+
+    channel_dict = {}
+    for col in d.columns:
+        if col.startswith("cycle"):
+            m = re.search("cycle(\d+)_channel(\d+)_(.*)", col)
+            channel_dict[(m.group(1), m.group(2))] = m.group(3)
+    nucleotide_codes = []
+    for gene_ind in d.index:
+        gene = d.loc[gene_ind]
+        str_l = []
+        for i, ind in enumerate(str(gene.code)):
+            ch_name = channel_dict[(str(i + 1), ind)]
+            col_name = f"cycle{i+1}_channel{ind}_{ch_name}"
+            assert gene[col_name] == 1
+            str_l.append(ch_name)
+        nucleotids = "".join([channel_map[s] for s in str_l])
+        nucleotide_codes.append(nucleotids)
+    d["nucleotide_codes"] = nucleotide_codes
+    channel_indexes = [int(k[1]) for k in channel_dict.keys()]
+    assert len(np.unique(channel_indexes)) == np.max(channel_indexes)
+    channel_info["nChannel"] = np.max(channel_indexes)
+    channel_info["DAPI"] = "nuclei"
+    for ch in channel_map:
+        if ch == "AF750":
+            channel_info["Atto425"] = channel_map[ch]
+        else:
+            channel_info[ch] = channel_map[ch]
+
+    df = pd.DataFrame(pd.Series(channel_info)).T
+    df.to_csv("channel_info.csv", index=False)
+    print(df)
+
+    taglist = d[["gene", "nucleotide_codes"]]
+    taglist = taglist.rename(columns={"gene": "Gene", "nucleotide_codes": "Channel"})
+    print(taglist)
+    pd.DataFrame(taglist).to_csv("taglist.csv", index=False)
+
+
+if __name__ == "__main__":
+    options = {
+        "run": main,
+        "version": VERSION 
+    }
+    fire.Fire(options)
