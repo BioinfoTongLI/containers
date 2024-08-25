b14d138ca50377a016579eb8610bfb45e65b52b3
Tong LI <tl10@sanger.ac.uk>
2024-08-25 21:26:59 +0000

    add lazy peak profile extraction

---
+++ extract_peak_profile/scripts/python/extract_peak_profile.py
@@ -0,0 +1,82 @@
+from aicsimageio import AICSImage
+import pandas as pd
+from skimage.morphology import disk
+import fire
+import numpy as np
+from shapely import from_wkt, MultiPoint
+from tqdm import tqdm
+
+
+VERSION = "0.0.1"
+
+def make_in_range(l, upper):
+    l = np.where(l < 0, 0, l)
+    l = np.where(l > upper, upper, l)
+    return l
+
+
+def main(image:str, peaks:str, stem:str,
+         pixelsize:float=1.0, peak_radius:int=4,
+         y_col:str='y_location',
+         x_col:str='x_location'):
+    print('Reading image and transcripts')
+    if peaks.endswith('.csv'):
+        spots = pd.read_csv(peaks, header=0, sep=',')[[y_col, x_col]].values
+    elif peaks.endswith('.tsv'):
+        spots = pd.read_csv(peaks, header=0, sep='\t')[[y_col, x_col]].values
+    elif peaks.endswith('.wkt'):
+        # Assuming that the wkt file contains a multipoint geometry
+        with open(peaks, 'r') as f:
+            multispots = from_wkt(f.read())
+        if not isinstance(multispots, MultiPoint):
+            raise ValueError('Please provide a wkt file with multipoint geometry')
+        spots = np.array([(geom.y, geom.x) for geom in multispots.geoms])
+    else:
+        raise ValueError('Format not recognized. Please provide a csv, tsv or wkt file')
+
+    # Convert the coordinates to pixel coordinates
+    spots = (spots/pixelsize).astype(np.int32)
+    
+    # Get pixel data
+    image = AICSImage(image)
+    image_stack = image.get_image_dask_data("CYX")
+    img_Y, img_X = image_stack.shape[-2:]
+    print(image_stack)
+
+    # Get the cooridnates of pixels surrounding the peaks 
+    disk_img = disk(peak_radius)
+    indices = np.where(disk_img == 1)
+    dXYpair = zip(indices[0] - peak_radius, indices[1] - peak_radius)
+
+    # Translate the coordinates of the peaks to the coordinates of the pixels surrounding the peaks
+    Xs, Ys = [], []
+    for dx, dy in dXYpair:
+        Ys.append(make_in_range(spots[:,0] + dy, img_Y - 1))
+        Xs.append(make_in_range(spots[:,1] + dx, img_X - 1))
+
+    peak_intensities = []
+    # Using for loop here to save memory, since dask doesnt' support fancy indexing yet.
+    # Otherwise we would use broadcasting to get the peak profiles
+    for ch in tqdm(range(image_stack.shape[0])):
+        peak_profile_surroundings = image_stack[ch].compute()[Ys, Xs]
+        max_intensity = np.max(peak_profile_surroundings, axis=0)
+        peak_intensities.append(max_intensity)
+    print(np.array(peak_intensities)[:, 0])
+    formatted_peak_profiles = np.transpose(
+        np.array(peak_intensities), (1, 0)
+    ).astype(np.int16)
+    print(formatted_peak_profiles.shape)
+    np.save(
+        f"{stem}_extracted_peak_intensities.npy",
+        formatted_peak_profiles,
+        allow_pickle=True,
+    )
+    pd.DataFrame(spots, columns=["y_int", "x_int"]).to_csv(f"{stem}_peak_locs.csv", index=False)
+        
+    
+if __name__ == "__main__":
+    options = {
+        "run": main,
+        "version": VERSION
+    }
+    fire.Fire(options)
