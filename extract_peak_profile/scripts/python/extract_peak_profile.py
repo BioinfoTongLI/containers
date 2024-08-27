7e18d0efddc04a90b2a0f4d4213fe9181251b5ba
Tong LI <tl10@sanger.ac.uk>
2024-08-27 18:37:21 +0000

    keep the original n_ch * n_spot as profile shape

--- extract_peak_profile/scripts/python/extract_peak_profile.py
+++ extract_peak_profile/scripts/python/extract_peak_profile.py
@@ -66,9 +66,7 @@ def main(image:str, peaks:str, stem:str,
         max_intensity = np.max(peak_profile_surroundings, axis=0)
         peak_intensities.append(max_intensity)
     print(np.array(peak_intensities)[:, 0])
-    formatted_peak_profiles = np.transpose(
-        np.array(peak_intensities), (1, 0)
-    ).astype(np.int16)
+    formatted_peak_profiles = np.array(peak_intensities).astype(np.int16)
     print(formatted_peak_profiles.shape)
     np.save(
         f"{stem}.npy",
