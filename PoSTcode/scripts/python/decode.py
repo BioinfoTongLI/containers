2ac832b5913b957ee4c0b2e491e29c1ef2beb031
Tong LI <tl10@sanger.ac.uk>
2024-08-29 08:51:12 +0000

    fixed reshaping issue.

--- PoSTcode/scripts/python/decode.py
+++ PoSTcode/scripts/python/decode.py
@@ -7,7 +7,10 @@ import fire
 from pathlib import Path
 from avg_spot_profile import main as average_spot_profiles
 from decoding_functions import decoding_function, decoding_output_to_dataframe
+import logging
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 VERSION="0.0.1"
 
@@ -48,7 +51,7 @@ def decode(spot_locations_p: str,
            readouts_csv: str=None,
            keep_noises=True,
            min_prob = 0.95,
-           R:int=6) -> pd.DataFrame:
+           R:int=None) -> pd.DataFrame:
     """
     Decodes spots using the Postcode algorithm.
 
@@ -60,7 +63,7 @@ def decode(spot_locations_p: str,
         keep_noises (bool, optional): Whether to keep spots that were classified as 'background' or 'infeasible'.
         min_prob: [0,1] - value of minimum allowed probability of decoded spot
             Defaults to True.
-        R (int, optional): Number of rounds. Defaults to 6.
+        R (int): Number of rounds. Defaults to None.
 
     Returns:
         pd.DataFrame: A pandas DataFrame containing the decoded spots and their locations.
@@ -69,7 +72,8 @@ def decode(spot_locations_p: str,
     spot_locations = pd.read_csv(spot_locations_p)
 
     spot_profile = np.load(spot_profile_p)
-    if len(spot_profile.shape) == 2:
+    if len(spot_profile.shape) == 2 and R:
+        logger.info("Reshaping spot profile for MERFISH-like data")
         # if the spot_profile is two dimensional, it is assumed that the spot_profile is in
         # the shape of (n_channel*n_cycle, n_spot). Then reshape it.
         n_ch, n_spots = spot_profile.shape
@@ -78,7 +82,10 @@ def decode(spot_locations_p: str,
         coding_mask_pre_cycle = np.ones(n_chs_per_cycle)
         coding_mask_pre_cycle[0] = 0 # remove DAPI/Hoechst channel
         coding_ch_mask = np.array(list(coding_mask_pre_cycle) * R, dtype=bool)
-        spot_profile = spot_profile[coding_ch_mask].reshape(n_spots, n_chs_per_cycle - 1, R)
+        spot_profile = spot_profile[coding_ch_mask].reshape(R, n_chs_per_cycle - 1, n_spots)
+        spot_profile = spot_profile.transpose(2, 1, 0)
+        # np.save(f"{stem}_reshaped_spot_profile.npy", spot_profile)
+        print(spot_profile.shape, spot_profile[0], type(spot_profile[0]), spot_profile[0].dtype)
 
     if readouts_csv:
         spot_profile, N_readouts = average_spot_profiles(spot_profile, readouts_csv) # Average is chosen over max for MERFISH-like profiles
@@ -87,10 +94,9 @@ def decode(spot_locations_p: str,
         gene_list, codebook_arr, K = ReadPrepCodebook_ISS(codebook_p)
 
     assert spot_locations.shape[0] == spot_profile.shape[0]
-    spot_profile = np.nan_to_num(spot_profile, nan=0)
+    print(spot_profile.shape)
     # Decode using postcode
     out = decoding_function(spot_profile, codebook_arr, print_training_progress=False)
-    
     
     # Reformat output into pandas dataframe
     df_class_names = np.concatenate((gene_list, ['infeasible', 'background', 'nan']))
