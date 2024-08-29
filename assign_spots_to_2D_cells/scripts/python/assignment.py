84d548bcb07cb0a06524d0e5a96e02ebc7ac5299
Tong LI <tl10@sanger.ac.uk>
2024-08-29 09:30:47 +0000

    flexible column names and logger for logging

--- assign_spots_to_2D_cells/scripts/python/assignment.py
+++ assign_spots_to_2D_cells/scripts/python/assignment.py
@@ -1,17 +1,25 @@
 from aicsimageio import AICSImage
 import pandas as pd
-import anndata
 import fire
+import logging
 
+logging.basicConfig(level=logging.INFO)
+
+logger = logging.getLogger(__name__)
 
 VERSION = "0.0.1"
 
-def main(label_image, transcripts, out_name, pixelsize=1):
-    print('Reading image and transcripts')
+def main(label_image:str, transcripts:str, out_name:str,
+         pixelsize:int=1,
+         y_col:str='y_location',
+         x_col:str='x_location',
+         feature_col:str='feature_name',
+    ):
+    logger.log('Reading image and transcripts')
     if transcripts.endswith('.csv'):
-        spots = pd.read_csv(transcripts, header=0, sep=',')[['y_location', 'x_location', 'feature_name']]
+        spots = pd.read_csv(transcripts, header=0, sep=',')[[y_col, x_col, feature_col]]
     elif transcripts.endswith('.tsv'):
-        spots = pd.read_csv(transcripts, header=0, sep='\t')[['y_location', 'x_location', 'feature_name']]
+        spots = pd.read_csv(transcripts, header=0, sep='\t')[[y_col, x_col, feature_col]]
     elif transcripts.endswith('.wkt'):
         from shapely import from_wkt, MultiPoint
         # Assuming that the wkt file contains a multipoint geometry
@@ -19,15 +27,15 @@ def main(label_image, transcripts, out_name, pixelsize=1):
             multispots = from_wkt(f.read())
         if not isinstance(multispots, MultiPoint):
             raise ValueError('Please provide a wkt file with multipoint geometry')
-        spots = pd.DataFrame([(geom.y, geom.x) for geom in multispots.geoms], columns=['y_location', 'x_location'])
+        spots = pd.DataFrame([(geom.y, geom.x) for geom in multispots.geoms], columns=[y_col, x_col])
         spots["feature_name"] = 'spot'
     else:
         raise ValueError('Format not recognized. Please provide a csv, tsv or wkt file')
     lab_2D = AICSImage(label_image)
-
-    print('Performing assignment')
+    logger.info('Assigning spots to cells')
     cell_id = lab_2D.dask_data[spots["y_location"].astype(int)/pixelsize, spots["x_location"].astype(int)/pixelsize]
     spots['cell_id'] = cell_id
+    logger.info("Create count matrix and save to file")
     count_matrix = spots.pivot_table(index='cell_id', columns='feature_name', aggfunc='size', fill_value=0)
     count_matrix = count_matrix.drop(count_matrix[count_matrix.index == 0].index)
     count_matrix.to_csv(out_name)
