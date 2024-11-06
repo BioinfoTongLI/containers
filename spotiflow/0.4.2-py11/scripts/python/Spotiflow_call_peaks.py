2df5cbc133415ec9a5e45bc0c493984c9cf69ba5
Tong LI <tl10@sanger.ac.uk>
2024-11-06 23:01:55 +0000

    translate if peaks were detected

--- spotiflow/0.4.2-py11/scripts/python/Spotiflow_call_peaks.py
+++ spotiflow/0.4.2-py11/scripts/python/Spotiflow_call_peaks.py
@@ -26,7 +26,6 @@ def main(image_path:str, out_dir:str, out_name:str,
     crop = lazy_one_plane[:, :, y_min:y_max, x_min:x_max].squeeze().compute()
     model = Spotiflow.from_pretrained(model_name)
     peaks, _  = model.predict(crop)
-    translated_peaks = translate(peaks, xoff=x_max, yoff=y_min)
 
 
     # Create the output directory if it doesn't exist
@@ -36,9 +35,9 @@ def main(image_path:str, out_dir:str, out_name:str,
     with open(f"{out_dir}/{out_name}", 'w', newline='') as f:
         writer = csv.writer(f)
         writer.writerow(['y', 'x'])  # write column names
-        if len(translated_peaks) > 0:
+        if len(peaks) > 0:
             # Serialize peaks to disk as CSV
-            writer.writerows(translated_peaks)
+            writer.writerows([translate(peaks[0], xoff=x_max, yoff=y_min)])
 
 
 if __name__ == "__main__":
